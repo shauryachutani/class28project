ca197afa51b401f8de8a462c858728ed4ce0e489
shauryachutani <61927538+shauryachutani@users.noreply.github.com>
2020-05-23 12:35:19 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,21 @@
+class Box{
+    constructor(x,y){
+        
+        this.body=Bodies.rectangle(x,y,20,20)
+        
+World.add(world,this.body)
+    }
+    display(){
+        var position=this.body.position
+        var angle =this.body.angle;
+        push()
+        rectMode(CENTER);
+        fill("blue")
+        translate(position.x,position.y)
+        angleMode(RADIANS)
+        rotate(angle)
+        rect(0,0,20,20);
+pop()
+        
+    }
+}
